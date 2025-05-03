490ea0a9a4ed3544ee25e4d41d79ed3f18fc3cb3
August-Trung <nguyenminhtrung01082003@gmail.com>
2025-05-03 23:59:19 +0700

    update connect to BE

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,16 +1,16 @@
 import axios, { AxiosInstance } from "axios";
 
-// Tạo instance axios với cấu hình cơ bản
+// Tạo instance axios với cấu hình động từ biến môi trường
 const instance: AxiosInstance = axios.create({
-	baseURL: "http://localhost:5000", // Đặt URL backend
+	baseURL: import.meta.env.REACT_APP_BE_URL, // URL backend từ biến môi trường
 	headers: {
-		"Content-Type": "application/json", // Đảm bảo gửi JSON
+		"Content-Type": "application/json",
 	},
 });
 
 // Log lỗi nếu xảy ra trong bất kỳ yêu cầu nào
 instance.interceptors.response.use(
-	(response) => response, // Trả về response nếu không có lỗi
+	(response) => response,
 	(error) => {
 		console.error("API Error:", error);
 		return Promise.reject(error);
